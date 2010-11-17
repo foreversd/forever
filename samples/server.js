d084ad10a051596cfea9b801d61e08de1e4f6687
indexzero <charlie.robbins@gmail.com>
2010-11-17 14:23:07 -0500

    [minor] Make samples/server.js listen on 8000

--- samples/server.js
+++ samples/server.js
@@ -5,7 +5,7 @@ http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.write('hello, i know nodejitsu.')
   res.end();
-}).listen(80);
+}).listen(8000);
 
 /* server started */  
 sys.puts('> hello world running on port 80');
