4b2524141c4da8772276cce6419f76dbf09bd293
indexzero <charlie.robbins@gmail.com>
2011-02-16 00:57:55 -0500

    [doc] Added example for chroot

---
+++ examples/chroot.js
@@ -0,0 +1,14 @@
+var sys = require('sys'),
+    daemon = require('daemon'),
+    fs = require('fs');
+
+daemon.chroot('/tmp/chroot');
+sys.puts('Working in chroot');
+
+setInterval(function () {
+  fs.readdir("./", function (err, files) {
+    sys.puts('Current directory: ' + process.cwd());
+    sys.puts('err: ' + sys.inspect(err));
+    sys.puts('files: ' + sys.inspect(files));
+  });
+}, 10000 );
