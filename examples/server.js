864b1d1df54c5881ea9ce597fa4c55a521e21f36
indexzero <charlie.robbins@gmail.com>
2011-04-29 03:54:20 -0400

    [minor] Small fixes

--- examples/server.js
+++ examples/server.js
@@ -5,8 +5,9 @@ var sys = require('sys'),
 var port = argv.p || argv.port || 80;
 
 http.createServer(function (req, res) {
+  console.log(req.method + ' request: ' + req.url);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('hello, i know nodejitsu.')
+  res.write('hello, i know nodejitsu.');
   res.end();
 }).listen(port);
 
