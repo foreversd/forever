6d93dcc1b41ccf976eb2767d408ecda3b3276fa0
Dusty Leary <dleary@gmail.com>
2011-05-30 15:18:39 -0700

    Add --spinSleepTime to throttle instead of killing spinning scripts

--- test/spin-test.js
+++ test/spin-test.js
@@ -17,16 +17,34 @@ var sys = require('sys'),
 vows.describe('forever/spin-restart').addBatch({
   "When using forever": {
     "and spawning a script that spin restarts": {
-      topic: function () {
-        var script = path.join(__dirname, '..', 'examples', 'always-throw.js'),
-            child = new (forever.Forever)(script, { silent: true });
+      "with no spinSleepTime specified": {
+        topic: function () {
+          var script = path.join(__dirname, '..', 'examples', 'always-throw.js'),
+              child = new (forever.Forever)(script, { silent: true, max: 3 });
 
-        child.on('exit', this.callback.bind(null, null));
-        child.start();
+          child.on('exit', this.callback.bind({}, null));
+          child.start();
+        },
+        "should spawn both processes appropriately": function (err, child, spinning) {
+          assert.isTrue(spinning);
+        },
+        "should not restart spinning processes": function (err, child, spinning) {
+          assert.equal(child.times, 0);
+        }
       },
-      "should spawn both processes appropriately": function (err, monitor, spinning) {
-        assert.isTrue(spinning);
+
+      "with a spinSleepTime specified": {
+        topic: function () {
+          var script = path.join(__dirname, '..', 'examples', 'always-throw.js'),
+              child = new (forever.Forever)(script, { silent: true, max: 3, spinSleepTime: 1 });
+
+          child.on('exit', this.callback.bind({}, null));
+          child.start();
+        },
+        "should restart spinning processes": function (err, child, spinning) {
+          assert.equal(child.times, 3);
+        }
       }
     }
-  },
+  }
 }).export(module);
