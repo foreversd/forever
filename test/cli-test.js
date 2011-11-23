60a576a58fd68cddddcb280ab8571ec88766dcaa
indexzero <charlie.robbins@gmail.com>
2011-11-22 19:29:46 -0500

    [test fix] Since forever.kill is async, use `async.forEach`. Update test/cli-test.js to rimraf ~/.forever temporarily

--- test/cli-test.js
+++ test/cli-test.js
@@ -11,12 +11,14 @@ var fs = require('fs'),
     assert = require('assert'),
     vows = require('vows'),
     helpers = require('./helpers'),
+    rimraf = require('utile').rimraf,
     forever = require('../lib/forever');
 
 var script = path.join(__dirname, '..', 'examples', 'log-on-interval.js');
 
 vows.describe('forever/cli').addBatch({
   'When using forever CLI': {
+<<<<<<< HEAD
     'and starting script using `forever start`': helpers.spawn(['start', script], {
       '`forever.list` result': helpers.list({
         'should contain spawned process': function (list) {
@@ -31,10 +33,30 @@ vows.describe('forever/cli').addBatch({
             'should not contain previous process': function (list) {
               assert.isNull(list);
             }
+=======
+    "when ~/.forever/sock is empty": {      
+      topic: function () {
+        rimraf(path.join(process.env.HOME, '.forever', 'sock'), this.callback);
+      },
+      'and starting script using `forever start`': helpers.spawn(['start', script], {
+        '`forever.list` result': helpers.list({
+          'should contain spawned process': function (list) {
+            helpers.assertList(list);
+            assert.equal(list[0].command, 'node');
+            assert.equal(fs.realpathSync(list[0].file), fs.realpathSync(script));
+            helpers.assertStartsWith(list[0].logFile, forever.config.get('root'));
+          },
+          'and stopping it using `forever stop`': helpers.spawn(['stop', script], {
+            '`forever.list` result': helpers.list({
+              'should not contain previous process': function (list) {
+                assert.isNull(list);
+              }
+            })
+>>>>>>> b622bf1... [test fix] Since forever.kill is async, use `async.forEach`. Update test/cli-test.js to rimraf ~/.forever temporarily
           })
         })
       })
-    })
+    }
   }
 }).export(module);
 
