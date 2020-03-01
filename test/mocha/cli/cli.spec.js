35716e5ab1c5c6317fd78d6ec03b2bc32ddee265
Igor Savin <iselwin@gmail.com>
2020-03-01 20:46:36 +0100

    Fix CI

--- test/mocha/cli/cli.spec.js
+++ test/mocha/cli/cli.spec.js
@@ -1,22 +1,24 @@
 const { execCommand } = require('cli-testlab');
-const rootDir = '.';
 
 describe('cli', () => {
   describe('start', () => {
-    it('starts script successfully', async () => {
+    it('starts script successfully', () => {
       const oldDir = process.cwd();
       process.chdir('test/mocha/cli/scripts/dir with spaces/');
-      await execCommand(
+      return execCommand(
         `node ../../../../../bin/forever start script_name.js`,
         {
-          expectedOutput: ['Forever processing file:', 'script_name.js']
+          expectedOutput: ['Forever processing file:', 'script_name.js'],
         }
-      );
-
-      await execCommand(`node ../../../../../bin/forever stopall`, {
-        expectedOutput: 'dir with spaces\\script_name.js',
-      });
-      process.chdir(oldDir);
+      )
+        .then(() => {
+          return execCommand(`node ../../../../../bin/forever stopall`, {
+            expectedOutput: 'dir with spaces\\script_name.js',
+          });
+        })
+        .then(() => {
+          process.chdir(oldDir);
+        });
     });
   });
 });
